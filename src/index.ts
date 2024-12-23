8bc51fce2e722eb1c34c77b9b20e7ed734223a15
011-Boom-Phongsakorn <664259011@webmail.npru.ac.th>
2024-12-23 15:04:22 +0700

    ex3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 const { Author, Book } = require('./Ex1')
+const { Customer, Invoice } = require('./Ex2')
+
 
 console.log('############## Ex1 ##############')
 
@@ -9,4 +11,16 @@ const author2 = new Author("za", "lnw@gmail.com")
 const book = new Book('Ty', [author1, author2], 999, 5)
 
 console.log(book.toString())
+console.log('#################################')
+
+
+console.log('############## Ex2 ##############')
+
+const customer1 = new Customer(1, 'Boom', 0.2)
+const invoice1 = new Invoice(1, customer1, 10000)
+
+console.log(invoice1.getAmountAfterDiscount())
+
+console.log(invoice1.toString())
+
 console.log('#################################')
