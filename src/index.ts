d92b5a3303ee91be37fb0a3f9c2ce6ee0cc22e11
011-Boom-Phongsakorn <664259011@webmail.npru.ac.th>
2024-12-23 13:23:23 +0700

    Ex2

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const { Author, Book } = require('./Ex1')
+
+console.log('############## Ex1 ##############')
+
+const author1 = new Author("Phongsakorn tangpok", "zny@gmail.com")
+const author2 = new Author("za", "lnw@gmail.com")
+
+
+const book = new Book('Ty', [author1, author2], 999, 5)
+
+console.log(book.toString())
+console.log('#################################')
